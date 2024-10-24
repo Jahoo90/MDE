3e5d448c68d7dcee1a7a20e2fda4ac519707c19b
Jahoo90 <jahoo1990@gmail.com>
2024-10-25 01:48:19 +0200

    header done and carousel in progress

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Strona załadowana.');
 });
+
+
+// document.getElementById('alertButton').addEventListener('click', () => {
+//     alert('Witaj, kliknąłeś przycisk!');
+// });
