819c6edfa897d8afe6a142b4db0e287f1303ac58
Jahoo90 <jahoo1990@gmail.com>
2024-11-05 21:18:12 +0100

    dark mode button styled

--- js/main.js
+++ js/main.js
@@ -90,3 +90,31 @@ function changeLanguage(lang) {
   alert(`Język zmieniony na: ${lang}`);
   // Tutaj można dodać logikę zmieniającą język na stronie
 }
+
+// ############################ Zmiana motywu ##############################
+
+const switchButton = document.querySelector("header button");
+let theme = localStorage.getItem("theme");
+
+switchButton.addEventListener("click", () => {
+  console.log("siema");
+    if (theme === "dark") {
+        document.querySelector("body").classList.remove("dark");
+        document.querySelector("body").classList.add("light");
+        theme = "light";
+    } else {
+        document.querySelector("body").classList.remove("light");
+        document.querySelector("body").classList.add("dark");
+        theme = "dark";
+    }
+
+    localStorage.setItem("theme", theme);
+});
+
+if (theme === "dark") {
+    document.querySelector("body").classList.add("dark");
+}
+
+if (theme === "light") {
+    document.querySelector("body").classList.add("light");
+}
